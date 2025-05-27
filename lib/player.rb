752c03be6f3de863b1a6d84ca1144616ba1d116c
melody-ste <melody_stephan@icloud.com>
2025-05-27 19:42:54 +0200

    modif player.rb

--- lib/player.rb
+++ lib/player.rb
@@ -32,3 +32,59 @@ class Player
   end
 
 end
+
+
+#joueur humain
+
+class HumanPlayer < Player 
+
+  attr_accessor :weapon_level
+
+  def initialize(name)
+    super(name)
+    @weapon_level = 1
+    @life_points = 100
+  end
+
+  def show_state
+    puts "#{@name} a #{@life_points} points de vie et une arme de niveau #{@weapon_level}"
+  end
+
+  def compute_damage
+  rand(1..6) * @weapon_level
+  end
+
+  def search_weapon
+  new_weapon = rand(1..6)
+
+  puts " tu as trouvé une arme de niveau #{new_weapon}"
+  
+  if new_weapon > @weapon_level
+    @weapon_level = new_weapon
+
+    puts "Youhou ! elle est meilleure que ton arme actuelle : tu la prends."
+  else 
+    puts "M@*#$... elle n'est pas mieux que ton arme actuelle..."
+
+  end
+
+  def search_health_pack
+    health_pack = rand(1..6)
+
+    if health_pack == 1
+     puts "Tu n'as rien trouvé..."
+
+    elsif health_pack >= 2 && health_pack <= 5
+     puts "Bravo tu as trouvé un pack de +50 points de vie !"
+     @life_points += 50
+     @life_points = 100 if @life_points > 100
+
+    else 
+      puts "Wouaw, tu as trouvé un pack de +80 points de vie !"
+      @life_points += 80
+      @life_points = 100 if @life_points > 100
+    end
+
+  end
+
+end
